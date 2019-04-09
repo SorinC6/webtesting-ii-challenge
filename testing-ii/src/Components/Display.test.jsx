cd442a37daf4faa7d40081257e4880e739df21be
Sorin Chis <kish.sorin@yahoo.com>
2019-04-09 16:19:24 +0300

    imported react testing library with npm and added jest-dom

--- testing-ii/src/Components/Display.test.jsx
+++ testing-ii/src/Components/Display.test.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import Display from "./Display";
 import * as rt from "react-testing-library";
+import "jest-dom/extend-expect";
 
 describe("<Display/> Component", () => {
   it("render without crasing", () => {
     rt.render(<Display />);
   });
 
-  it("display balls title", () => {
+  test("display balls title", () => {
     const display = rt.render(<Display />);
-    //display.getByTestId("balls");
-    expect(display.getByText(/Balls Count/i));
-    //expect(ballsCount).toHaveTextContent(/balls count/i);
+    const ballsCount = display.getByTestId(/balls/i);
+    expect(ballsCount).toHaveTextContent(/balls count/i);
+  });
+
+  test("display strikes title", () => {
+    const display = rt.render(<Display />);
+    const strikesCount = display.getByTestId("strikes");
+    expect(strikesCount).toHaveTextContent(/strikes count/i);
   });
 });
