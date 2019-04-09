d6ca87356c538f135736126d206b97ae7fd3be39
Sorin Chis <kish.sorin@yahoo.com>
2019-04-09 15:55:32 +0300

    added test for added 1 when ball button is pressed

--- testing-ii/src/Components/Display.jsx
+++ testing-ii/src/Components/Display.jsx
@@ -2,14 +2,37 @@ import React, { Component } from "react";
 import Dashboad from "./Dashbord";
 
 class Display extends Component {
-  state = {};
+  state = {
+    balls: 0,
+    strikes: 0
+  };
+
+  ballHandler = () => {
+    // this.setState({
+    //   balls: this.state.balls + 1
+    // });
+
+    this.setState(prevState => ({
+      balls: prevState.balls + 1
+    }));
+
+    if (this.state.balls === 4) {
+      this.setState({
+        balls: 0
+      });
+    }
+  };
 
   render() {
     return (
       <div>
         <h2>Display</h2>
+        <p data-testid="balls">Balls Count</p>
+        <p data-testid="balls-count">{this.state.balls}</p>
+        <p data-testid="strikes">Strikes Count</p>
+        <p data-testid="strikes-count">{this.state.strikes}</p>
         <div>
-          <Dashboad />
+          <Dashboad ballHandler={this.ballHandler} />
         </div>
       </div>
     );
