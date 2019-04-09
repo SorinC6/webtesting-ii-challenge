52fb69b668549da83586a337dcd3a28cad21bc4f
Sorin Chis <kish.sorin@yahoo.com>
2019-04-09 16:31:07 +0300

    added tests for dysplay ball count and strikes count, and improuve ball button tests

--- testing-ii/src/Components/Display.test.jsx
+++ testing-ii/src/Components/Display.test.jsx
@@ -19,4 +19,16 @@ describe("<Display/> Component", () => {
     const strikesCount = display.getByTestId("strikes");
     expect(strikesCount).toHaveTextContent(/strikes count/i);
   });
+
+  test("display balls count", () => {
+    const display = rt.render(<Display />);
+    const ballsCount = display.getByTestId(/balls-count/i);
+    expect(ballsCount).toHaveTextContent("0");
+  });
+
+  test("display strikes count", () => {
+    const display = rt.render(<Display />);
+    const strikes = display.getByTestId(/strikes-count/i);
+    expect(strikes).toHaveTextContent("0");
+  });
 });
